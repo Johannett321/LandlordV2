297ca3b6354afaf5d3057408aef3cd3764aad0ce
Johan <johan.svartdal30@gmail.com>
2023-03-24 17:53:18 +0100

    Updated IcyHillsEvent yaw and pitch

--- src/com/johansvartdal/landlord/events/adventure/IcyHillsEvent.java
+++ src/com/johansvartdal/landlord/events/adventure/IcyHillsEvent.java
@@ -7,14 +7,13 @@ import org.bukkit.Location;
 
 public class IcyHillsEvent extends AdventureEvent{
 
-    //TODO update location, yaw and pitch
     private final Location advLocation = new Location(Bukkit.getWorld("lladv"), 217, 157 ,-1408);
     private final int excursionMinutes = 5;
 
     public IcyHillsEvent(Main plugin) {
         super(plugin);
-        advLocation.setYaw(-172);
-        advLocation.setPitch(51);
+        advLocation.setYaw(150);
+        advLocation.setPitch(-9);
     }
 
     @Override
@@ -29,7 +28,6 @@ public class IcyHillsEvent extends AdventureEvent{
 
     @Override
     protected void showWelcomeMessage() {
-        //TODO change name of place
         God.speak("Greetings and welcome to Frostgarde's mountains! There are some hidden treasures scattered throughout the mountain. " +
                 "Find them before your " + excursionMinutes + " minute excursion ends. Perhaps you'll uncover a valuable book of mending?");
     }
