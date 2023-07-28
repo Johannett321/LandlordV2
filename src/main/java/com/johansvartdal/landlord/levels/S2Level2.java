c232332766aa11c5ea14513ae203d64c83f194b6
hdjs <johan.svartdal@husbanken.no>
2023-07-28 10:40:48 +0200

    Updated requirements for S2L1 og S2L2

--- src/main/java/com/johansvartdal/landlord/levels/S2Level2.java
+++ src/main/java/com/johansvartdal/landlord/levels/S2Level2.java
@@ -8,9 +8,9 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 
-public class S2Level1 extends Level{
+public class S2Level2 extends Level{
 
-    public S2Level1(Main plugin) {
+    public S2Level2(Main plugin) {
         super(plugin, 2, 1);
     }
 
@@ -19,20 +19,19 @@ public class S2Level1 extends Level{
         ArrayList<ItemStack> requiredItems = new ArrayList<>();
 
         //TODO NOT DONE!!
-        requiredItems.add(new ItemStack(Material.TUFF, 40 * Main.properties.getNumberOfPlayers()));
+        requiredItems.add(new ItemStack(Material.FLINT, 34 * Main.properties.getNumberOfPlayers()));
+        requiredItems.add(new ItemStack(Material.POINTED_DRIPSTONE, 23 * Main.properties.getNumberOfPlayers()));
+        // CHERRY: requiredItems.add(new ItemStack(Material.CHERRY_LOG, 23 * Main.properties.getNumberOfPlayers()));
 
-        requiredItems.add(new ItemStack(Material.POTATO, 240 * Main.properties.getNumberOfPlayers()));
-        requiredItems.add(new ItemStack(Material.CARROT, 128 * Main.properties.getNumberOfPlayers()));
-        requiredItems.add(new ItemStack(Material.SEA_PICKLE, 64 * Main.properties.getNumberOfPlayers()));
-        requiredItems.add(new ItemStack(Material.PUMPKIN, 64 * Main.properties.getNumberOfPlayers()));
+
+        requiredItems.add(new ItemStack(Material.KELP, 64 * Main.properties.getNumberOfPlayers()));
+        requiredItems.add(new ItemStack(Material.MILK_BUCKET, 4 * Main.properties.getNumberOfPlayers()));
         requiredItems.add(new ItemStack(Material.COCOA_BEANS, 256 * Main.properties.getNumberOfPlayers()));
+        requiredItems.add(new ItemStack(Material.WHITE_WOOL, 100 * Main.properties.getNumberOfPlayers()));
 
-        requiredItems.add(new ItemStack(Material.OAK_LOG, 176 * Main.properties.getNumberOfPlayers()));
-        requiredItems.add(new ItemStack(Material.OAK_LEAVES, 144 * Main.properties.getNumberOfPlayers()));
-
-        requiredItems.add(new ItemStack(Material.EGG, 24));
-        requiredItems.add(new ItemStack(Material.WHITE_WOOL, 21 * Main.properties.getNumberOfPlayers()));
-
+        requiredItems.add(new ItemStack(Material.WARPED_WART_BLOCK, 19 * Main.properties.getNumberOfPlayers()));
+        requiredItems.add(new ItemStack(Material.SHROOMLIGHT, 4 * Main.properties.getNumberOfPlayers()));
+        requiredItems.add(new ItemStack(Material.WEEPING_VINES, 20 * Main.properties.getNumberOfPlayers()));
 
         // COPPER ORE
         // TUFF fant jeg veldig dypt! -45Y. Denne er ganske vanlig.
